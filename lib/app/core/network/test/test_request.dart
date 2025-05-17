b000f5b2df41e4506e5a4f12e0fa154a92d13878
KSK9820 <KSK9820@gmail.com>
2025-05-17 16:08:26 +0900

    [chore]: 네트워크 예시 코드

---
+++ lib/app/core/network/test/test_request.dart
@@ -0,0 +1,31 @@
+import 'package:easip_app/app/core/config/env_config.dart';
+import 'package:easip_app/app/core/network/api_request.dart';
+import 'package:easip_app/app/core/network/test/test_model.dart';
+
+final class TestRequest<T> extends ApiRequest<T> {
+    @override
+    String get baseUrl => EnvConfig().baseUrl;
+
+    @override
+    String get path => '/posts';
+
+    @override
+    String get method => HttpMethod.get.value;
+
+    @override
+    Map<String, String>? get headers => null;
+
+    @override
+    Map<String, dynamic>? get queryParameters => null;
+
+    @override
+    dynamic get body => null;
+
+    @override
+    T parseResponse(dynamic data) {
+        if (data is List) {
+            return data.map((item) => TestModel.fromJson(item as Map<String, dynamic>)).toList() as T;
+        }
+        throw FormatException('Expected List but got ${data.runtimeType}');
+    }
+}
