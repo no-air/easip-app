c4bed3f282a6be7270a547b3c42e9e93254a38a3
loopy-lim <cotmd6203@naver.com>
2025-05-28 16:35:06 +0900

    feat: add webview setting

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,22 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:get/get.dart';
 import 'app/routes/app_pages.dart';
 import 'app/routes/app_routes.dart';
 import 'app/theme/app_colors.dart';
 import 'app/services/auth_service.dart';
 
-void main() {
+WebViewEnvironment? webViewEnvironment;
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+
+  if (!kIsWeb && defaultTargetPlatform == TargetPlatform.android) {
+    await InAppWebViewController.setWebContentsDebuggingEnabled(kDebugMode);
+  }
   Get.put(AuthService());
   runApp(const App());
 }
