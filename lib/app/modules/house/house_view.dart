ad45ca2d92bed030f596fb136ddc300768267ca3
loopy-lim <cotmd6203@naver.com>
2025-06-04 14:01:40 +0900

    feat: add house view

---
+++ lib/app/modules/house/house_view.dart
@@ -0,0 +1,15 @@
+import 'package:easip_app/app/components/webview/webview.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:get/get.dart';
+
+class HouseView extends GetView {
+  HouseView({super.key});
+  final String baseUrl = dotenv.env['WEBVIEW_URL'] ?? '';
+  final String houseId = Get.parameters['houseId'] ?? '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Webview(url: '$baseUrl/house/$houseId'));
+  }
+}
